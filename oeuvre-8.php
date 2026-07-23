e761cf1ae528388736e0a4f2fda6dfc59a32bc76
fncln <francolon/mickael@gmail.com>
2026-07-23 12:16:18 +0200

    feat: mise en place du projet et factorisation du header et footer (Etapes 1 et 2)

--- oeuvre-8.php
+++ oeuvre-8.php
@@ -1,22 +1,6 @@
-<!doctype html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
-    <title>The ArtBox</title>
-</head>
-<body>
-<header>
-    <a href="index.html"><img src="img/logo.png" alt="Logo Artbox" id="logo"></a>
-    <nav>
-        <ul>
-            <li><a href="index.html">Accueil</a></li>
-        </ul>
-    </nav>
-</header>
+<?php
+include_once ('includes/header.php');
+?>
 <main>
     <article id="detail-oeuvre">
         <div id="img-oeuvre">
@@ -31,10 +15,6 @@
         </div>
     </article>
 </main>
-<footer>
-    <p>
-        <strong>© THE ARTBOX</strong> - <em>Tous droits réservés</em>
-    </p>
-</footer>
-</body>
-</html>
+<?php 
+include_once ('includes/footer.php');
+?> 
